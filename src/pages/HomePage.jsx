c8dd16d4bae970f579a67a14bb3460423ea18be5
kushtriimhyseni <kushtrim@starlabspro.com>
2022-04-19 03:40:31 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, Suspense, lazy } from "react";
 import Header from "../components/header/Header";
-import Banner from "../components/Banner";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import ScrollToTop from "../components/ScrollToTop";
 import Resume from "../components/Resume";
 import Portfolio from "../components/Portfolio";
 import Footer from "../components/Footer";
+
+const Banner = lazy(() => import("../components/Banner"));
 const HomePage = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <>
       <Header menuOpen={isMenuOpen} />
       <div className="pl-0 lg:pl-[300px]">
-        <Banner setOpen={setMenuOpen} menuOpen={isMenuOpen} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Banner setOpen={setMenuOpen} menuOpen={isMenuOpen} />
+        </Suspense>
         <About />
         <Skills />
         <Resume />
